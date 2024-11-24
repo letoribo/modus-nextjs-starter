7197ea7a5a16544cfc9d6fb2a158c851994e527a
yuriy_chistyakov <yuchistyakov@gmail.com>
2024-11-24 12:42:23 +0200

    my free trial ends

--- app/api/graphql/route.js
+++ app/api/graphql/route.js
@@ -2,47 +2,14 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import typeDefs from '@/lib/typeDefs.js';
-const token = process.env.token;
-const hypermode_url = process.env.hypermode_url || "https://proj-self.hypermode.app/graphql";
 
 const resolvers = {
   Query: {
-    greeting: async (_, { name }) => {
-      try {
-        const response = await fetch(hypermode_url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-          },
-          body: JSON.stringify({
-            /* variables: { name },
-            query: '{query sayHello($name:String){sayHello(name: $name)}}', */
-            query: `{sayHello(name: "${name}")}`,
-          })
-        })
-        const data = await response.json();
-        console.log(data)
-        return data.data;
-      } catch (error) {
-        console.error("error:", error);
-      }
-    },
+    greeting: async (_, { name }) => ({ sayHello: `Hello, ${name}` }),
     getQuote: async () => {
-      const response = await fetch(hypermode_url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({
-          //variables: {},
-          query: '{ randomQuote { author quote } }',
-        })
-      })
-      const data = await response.json();
-      console.log(data)
-      return data.data.randomQuote;
+      const response = await fetch('https://zenquotes.io/api/random')
+      const [data] = await response.json();
+      return { quote: data.q, author: data.a };
     }
   },
 }
